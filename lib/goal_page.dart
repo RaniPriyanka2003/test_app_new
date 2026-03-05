1af4a7cb3abc87b467fc9945d1ea782db7ad200b
RaniPriyanka2003 <ranipriyankachennam933@gmail.com>
2026-03-05 15:09:11 +0530

    Create goal_page.dart

---
+++ lib/goal_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class GoalPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(" ")),
+      body: Center(
+        child: Text("Welcome To Goal Page", style: TextStyle(fontSize: 22)),
+      ),
+    );
+  }
+}
+
