ede416a8fe11d8c91d4604a8855b7370acc978f4
Priyanka <ranipriyankachennam933@gmail.com>
2026-03-06 11:27:11 +0530

    changed colour

---
+++ lib/showdailouge.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: LoginPage(),
+    );
+  }
+}
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Login Page"),
+        centerTitle: true,
+      ),
+
+      body: Center(
+        child: ElevatedButton(
+          child: const Text("Login"),
+          onPressed: () {
+            Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const HomePage(),
+              ),
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  void showLogoutDialog(BuildContext context) {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: const Text("Logout"),
+          content: const Text("Are you sure you want to log out?"),
+
+          actions: [
+            TextButton(
+              onPressed: () {
+                Navigator.pop(context); 
+              },
+              child: const Text("Cancel"),
+            ),
+
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pushAndRemoveUntil(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const LoginPage(),
+                  ),
+                  (route) => false,
+                );
+              },
+              child: const Text("Proceed"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home Page"),
+        centerTitle: true,
+
+        actions: [
+          InkWell(
+            onTap: () {
+              showLogoutDialog(context);
+            },
+            child: const Padding(
+              padding: EdgeInsets.only(right: 12),
+              child: Row(
+                children: [
+                  Icon(Icons.logout),
+                  SizedBox(width: 5),
+                  Text("Logout"),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+
+      body: const Center(
+        child: Text(
+          "Welcome to Home Page",
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
